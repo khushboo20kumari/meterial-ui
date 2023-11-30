4ac419b539b636f27346e376c985569f39d7ad6e
Khushboo Kumari <khushboo20@navgurukul.org>
2023-11-30 22:58:23 +0530

    add onChange in select option

--- src/SelectArray.js
+++ src/SelectArray.js
@@ -1,13 +1,27 @@
+import { useState } from "react";
+
 function SelectArray() {
     const data=["completed","pending","not started","started"]
+
+    const [str,setStr]=useState("")
+
+    const HandleChange=(e)=>{
+        setStr(e.target.value)
+        
+
+    }
+    console.log(str)
     return (
         <>
             hello Select Arrray data.....
-            <select>
+            <select onChange={HandleChange}>
                 {data.map((item)=>(
                     <option>{item}</option>
                 ))}
+               
             </select>
+            <p>{str}</p>
+
 
         </>
     )
